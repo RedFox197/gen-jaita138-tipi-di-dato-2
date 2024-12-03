0abb8d6e579f5be573dac323d8390110ab9893d0
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2024-12-03 17:07:34 +0100

    Dimenticato che la data di iscrizione è un LocalDateTime

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,10 +1,12 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HH:mm");
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Inserisci il nome: ");
@@ -14,7 +16,7 @@ public class Cliente {
         String cognome = scanner.nextLine();
 
         System.out.println("Inserisci la data di nascita: (giorno/mese/anno) a numeri");
-        LocalDate dataNascita = LocalDate.parse(scanner.nextLine(), dateTimeFormatter);
+        LocalDate dataNascita = LocalDate.parse(scanner.nextLine(), dateFormatter);
 
         System.out.println("Inserisci il numero di telefono: ");
         String nTelefono = scanner.nextLine();
@@ -32,12 +34,12 @@ public class Cliente {
         String username = scanner.nextLine();
 
         System.out.println("Inserisci data di iscrizione (giorno/mese/anno) a numeri: ");
-        LocalDate dataIscrizione = LocalDate.parse(scanner.nextLine(), dateTimeFormatter);
+        LocalDateTime dataIscrizione = LocalDateTime.parse(scanner.nextLine(), dateTimeFormatter);
 
         System.out.println("RIEPILOGO DATI INSERITI: ");
         System.out.printf(
                 "Nome: %s\nCognome: %s\nData di Nascita: %s\nNumero di Telefono: %s\nNote: %s\nEmail: %s\nPassword: %s\nUsername: %s\nData di Iscrizione: %s\n",
-                nome, cognome, dataNascita.format(dateTimeFormatter), nTelefono, note, email, password, username, dataIscrizione.format(dateTimeFormatter)
+                nome, cognome, dataNascita.format(dateFormatter), nTelefono, note, email, password, username, dataIscrizione.format(dateTimeFormatter)
         );
         scanner.close();
     }
